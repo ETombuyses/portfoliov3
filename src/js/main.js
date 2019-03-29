64146077ad8b1fe14e9d245a01731757a0950cb6
ETombuyses <e.tombuyses@gmail.com>
2019-03-29 16:20:02 +0100

    flipcard on click and mouseover

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,28 @@
-
+/* --------------  Custom cursor ------------------ */
 var CursorPosition = require('./cursor.js');
 
 let cursor = document.querySelector('.cursor');
 let cursorPos = new CursorPosition(cursor);
 
 
+
+/* ------------- Flipcard on mouseover -------------- */
+
+const flipcard = document.getElementById('flipcard');
+const flipcardInner = flipcard.querySelector('.flipcard__inner');
+
+setTimeout( () => {
+  flipcardInner.classList.add('to-back');
+}, 1000);
+
+document.addEventListener('mousemove', () => {
+  
+  flipcard.addEventListener('mouseover', () => {
+    flipcardInner.classList.toggle('to-back');
+  });
+
+});
+
+flipcard.addEventListener('click', () => {
+  flipcardInner.classList.toggle('to-back');
+});
