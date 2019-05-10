af1e3170b74222d7ba8f64b6b9aad8edaca96d42
ETombuyses <e.tombuyses@gmail.com>
2019-05-10 17:36:15 +0200

    new work display

---
+++ src/js/projects.js
@@ -0,0 +1,131 @@
+
+let projects = [
+  {
+    title: "Tower of Hanoi",
+    descriptions: [
+      "A well known mathematical game that I coded.",
+      "The rules are explained on the game's page.",
+      "Have fun playing it !"
+    ],
+    link : "https://tower-of-hanoi.netlify.com/",
+    image: "hanoi.33374933.svg",
+    alt: "tower of Hanoi",
+    cta: "Game"
+  },
+  {
+    title: "Nespresso redesign",
+    descriptions: [
+      "This project was for my UX course. I had to redesign the homepage of Nespresso. But I didn’t stop there!",
+      "I really wanted to make this redesign live, so I did. (I am still working on it)."
+    ],
+    link : "https://nespresso-redesign-school-project.netlify.com/",
+    image: "tasse.f1029df2.png",
+    alt: "nespresso redesign",
+    cta: "website"
+  },
+  {
+    title: "Yellow Jacket",
+    descriptions: [
+      "Me and my team had 5 days to develop and present a javascript game in front of a jury. It was a great project where I improved my JS skills a lot. It was a lot of fun too!",
+      "<strong>To play in a 1280x800 environment</strong>"
+    ],
+    link : "https://yellowjacketgame.netlify.com/",
+    image: "yellow-jacket.a7912945.png",
+    alt: "javascript game - yellow jacket",
+    cta: "game"
+  },
+  {
+    title: "Emporio Armani",
+    descriptions: [
+      "This was my first integration after a few weeks of courses, also made in 5 days including the design and a presentation in front of a jury.",
+      "I integrated the home page and made the responsive part for product pages and the home page."
+    ],
+    link : "https://armani-soufre.netlify.com/",
+    image: "armani-montre.41d698f8.png",
+    alt: "emporio armani integration",
+    cta: "website"
+  }
+];
+
+
+let works = document.getElementById("works");
+let fragment = document.createDocumentFragment();
+
+const displayWorks = () => {
+  for (let i = 0; i < projects.length; i++) {
+
+    //article
+    let work = document.createElement('article');
+    work.classList.add('work');
+
+    if(i%2 == 0) {
+      work.classList.add('work--odd');
+    }
+
+    /*section description*/
+    let description = document.createElement('section');
+    description.classList.add('work__description');
+
+    //tite
+    let title = document.createElement('h3');
+    title.classList.add('work__title');
+    title.textContent = projects[i].title;
+    description.appendChild(title);
+
+    //texts
+    let texts = projects[i].descriptions;
+
+    for (let j = 0; j < texts.length; j++) {
+      text = document.createElement('p');
+      text.classList.add('work__text');
+      text.innerHTML = texts[j];
+      description.appendChild(text);
+    }
+
+    /*section display*/
+    let display = document.createElement('section');
+    display.classList.add('work__display');
+    
+    //link 
+    let link = document.createElement('a');
+    link.classList.add('work__bigBox');
+    link.href = projects[i].link;
+    link.target = "_blank";
+
+    link.innerHTML= `
+    <div class="work__box">
+      <div class="work__filter">
+        <span class="work__tags">${projects[i].cta}</span>
+      </div>
+      <img src=${projects[i].image} alt="${projects[i].alt}">
+    </div>`;
+
+    display.appendChild(link);
+
+
+
+    /*combine sections*/
+    work.appendChild(description);
+    work.appendChild(display);
+
+
+    // add to fragment
+    fragment.appendChild(work);
+  }
+}
+
+displayWorks();
+works.appendChild(fragment);
+
+
+
+// <section class="work__display">
+//   <a href="">
+//     <div class="work__box">
+//       <div class="work__filter">
+//         <span class="work__tags">Website</span>
+//       </div>
+//       <img src="tasse.f1029df2.png" alt="nespresso-redesign">
+//     </div>
+//   </a>
+// </section>
